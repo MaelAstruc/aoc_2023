97fa7e250c5b263839f7bfa2c09ad82396a844d0
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2023-12-01 19:37:24 +0100

    day 1 V2
    Less functional but proper matching to handle the number names

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -20,7 +20,20 @@ fn main() {
             7pqrstsixteen"
         ),
         281
-    )
+    );
+
+    assert_eq!(
+        part2_v2(
+            "two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen"
+        ),
+        281
+    );
 }
 
 fn part1(input: &str) -> u32 {
@@ -78,4 +91,90 @@ fn part2(input: &str) -> u32 {
         .sum();
     println!("{result}");
     result
+}
+
+fn part2_v2(input: &str) -> u32 {
+    let mut first: Option<u32> = None;
+    let mut last: Option<u32> = None;
+    let mut total: u32 = 0;
+
+    for (i, c) in input.char_indices() {
+        match c {
+            '1'..='9' => {
+                (first, last) = update(first, last, c.to_string().parse::<u32>().unwrap())
+            }
+            'o' => {
+                if check_number_name(input, i, "one") {
+                    (first, last) = update(first, last, 1)
+                }
+            }
+            't' => {
+                if check_number_name(input, i, "two")  {
+                    (first, last) = update(first, last, 2)
+                }
+                else if check_number_name(input, i, "three")  {
+                    (first, last) = update(first, last, 3)
+                }
+            }
+            'f' => {
+                if check_number_name(input, i, "four") {
+                    (first, last) = update(first, last, 4)
+                }
+                else if check_number_name(input, i, "five")  {
+                    (first, last) = update(first, last, 5)
+                }
+            }
+            's' => {
+                if check_number_name(input, i, "six") {
+                    (first, last) = update(first, last, 6)
+                }
+                else if check_number_name(input, i, "seven") {
+                    (first, last) = update(first, last, 7)
+                }
+            }
+            'e' => {
+                if check_number_name(input, i, "eight") {
+                    (first, last) = update(first, last, 8)
+                }
+            }
+            'n' => {
+                if check_number_name(input, i, "nine") {
+                    (first, last) = update(first, last, 9)
+                }
+            }
+            '\n' => {
+                total += new_value(first, last);
+                (first, last) = (None, None)
+            }
+            _ => (),
+        }
+    }
+    
+    // For the last line
+    total += new_value(first, last);
+
+    println!("{total}");
+    total
+}
+
+fn update(first: Option<u32>, last: Option<u32>, value: u32) -> (Option<u32>, Option<u32>) {
+    match (first, last) {
+        (Some(_), Some(_)) => (first, Some(value)),
+        (None, None) => (Some(value), Some(value)),
+        (Some(_), None) => unreachable!(),
+        (None, Some(_)) => unreachable!(),
+    }
+}
+
+fn check_number_name(input: &str, i: usize, name: &str) -> bool{
+    i + name.len() <= input.len() && &input[i..(i+name.len())] == name
+}
+
+fn new_value(first: Option<u32>, last: Option<u32>) -> u32 {
+    match (first, last) {
+        (Some(x), Some(y)) => x * 10 + y,
+        (None, None) => 0,
+        (Some(_), None) => unreachable!(),
+        (None, Some(_)) => unreachable!(),
+    }
 }
